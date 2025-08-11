c44a20dbfff6e0b91653b7a9d93f872a9215c6f2
Luke Childs <lukechilds123@gmail.com>
2025-08-11 15:45:45 +0100

    Flesh out proof attestations

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,12 +1,18 @@
 use risc0_zkvm::guest::env;
 
 fn main() {
-    // TODO: Implement your guest code here
-
     // read the input
     let input: u32 = env::read();
 
-    // TODO: do something with the input
+    // TODO: assert signature is valid for message and pubkey
+
+    // TODO: assert derived address from pubkey is included at expected tx output index
+
+    // TODO: assert tx inclusion proof is valid for block header
+
+    // TODO: assert block inclusion proof is valid for header merkle root
+
+    // TODO: commit time from block header (rounded down to some interval)
 
     // write public output to the journal
     env::commit(&input);
