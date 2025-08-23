1ce5fc7913e1173297a7d651a4718abd03493d54
Luke Childs <lukechilds123@gmail.com>
2025-08-23 23:19:19 +0100

    Remove tracing

--- host/src/main.rs
+++ host/src/main.rs
@@ -89,11 +89,6 @@ fn serialize_receipt(receipt: &Receipt) -> String {
 
 #[tokio::main]
 async fn main() {
-    // Initialize tracing. In order to view logs, run `RUST_LOG=info cargo run`
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::filter::EnvFilter::from_default_env())
-        .init();
-
     println!("og-zkp");
 
     // User input
