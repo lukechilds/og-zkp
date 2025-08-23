c6852a1de4a0548ffbcee706353def68f9a67051
Luke Childs <lukechilds123@gmail.com>
2025-08-23 20:31:26 +0100

    Assert pubkey is in tx and tx is in a block

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,9 +1,10 @@
+use bitcoin::{consensus, script::ScriptBuf, MerkleBlock, Transaction, Txid};
 use ogzkp_core::{recover_pubkey_from_bitcoin_signed_message, OGZKP_MESSAGE_PREFIX};
 use risc0_zkvm::guest::env;
 
 fn main() {
     // Read the input
-    let (message, signature_bytes, _tx, _spv_proof): (String, Vec<u8>, String, String) =
+    let (message, signature_bytes, tx_hex, spv_proof_hex): (String, Vec<u8>, String, String) =
         env::read();
 
     // Assert message starts with "og-zkp"
@@ -14,12 +15,33 @@ fn main() {
     );
 
     // Recover pubkey from the signed message
-    let _pubkey = recover_pubkey_from_bitcoin_signed_message(&signature_bytes, &message)
+    let pubkey = recover_pubkey_from_bitcoin_signed_message(&signature_bytes, &message)
         .expect("Failed to recover pubkey from bitcoin signed message");
 
-    // TODO: Assert pubkey is a P2PKH output at expected outpoint
+    // Decode transaction hex
+    let tx_bytes = hex::decode(&tx_hex).expect("Invalid transaction hex");
+    let tx: Transaction =
+        consensus::encode::deserialize(&tx_bytes).expect("Failed to parse transaction");
 
-    // TODO: assert tx inclusion proof is valid for block header
+    // Assert pubkey is a P2PKH output in the transaction
+    let expected_output: ScriptBuf = ScriptBuf::new_p2pkh(&pubkey.pubkey_hash());
+    let tx_has_expected_output = tx.output.iter().any(|o| o.script_pubkey == expected_output);
+    assert!(
+        tx_has_expected_output,
+        "Recovered pubkey is not a P2PKH output in the transaction"
+    );
+
+    // Assert tx inclusion proof is valid for block header
+    let txid = tx.compute_txid();
+    let spv_proof_bytes = hex::decode(&spv_proof_hex).expect("Invalid SPV proof hex");
+    let spv_proof: MerkleBlock =
+        consensus::encode::deserialize(&spv_proof_bytes).expect("Failed to parse SPV proof");
+    let mut matches: Vec<Txid> = vec![];
+    let mut _indexes: Vec<u32> = vec![];
+    spv_proof
+        .extract_matches(&mut matches, &mut _indexes)
+        .expect("Failed to extract matches from SPV proof");
+    assert!(matches.contains(&txid), "Transaction is not in SPV proof");
 
     // TODO: assert block inclusion proof is valid for header merkle root
 
