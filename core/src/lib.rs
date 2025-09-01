8bc5f04682aaa003ed42e14e7b6fca2bf17e7380
Luke Childs <lukechilds123@gmail.com>
2025-09-01 06:22:19 +0100

    Move guest methods out of shared lib

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,34 +1,5 @@
 // TODO: Move these into main modules or inline them
 
-pub const OGZKP_MESSAGE_PREFIX: &str = "og-zkp ";
-
-use bitcoin::secp256k1::Secp256k1;
-use bitcoin::sign_message::{signed_msg_hash, MessageSignature, MessageSignatureError};
-use bitcoin::PublicKey;
-pub fn recover_pubkey_from_bitcoin_signed_message(
-    signature_bytes: &[u8],
-    message: &str,
-) -> Result<PublicKey, MessageSignatureError> {
-    let signature = MessageSignature::from_slice(signature_bytes)?;
-    let hash = signed_msg_hash(message);
-    let secp = Secp256k1::verification_only();
-    let pubkey = signature.recover_pubkey(&secp, hash)?;
-
-    Ok(pubkey)
-}
-
-use time::{Date, OffsetDateTime};
-pub fn start_of_month(unix_ts: u32) -> u32 {
-    let ts = OffsetDateTime::from_unix_timestamp(unix_ts.into()).unwrap();
-
-    let date = Date::from_calendar_date(ts.year(), ts.month(), 1).unwrap();
-
-    date.with_hms(0, 0, 0)
-        .unwrap()
-        .assume_utc()
-        .unix_timestamp() as u32
-}
-
 // Block inclusion proof methods
 
 // TODO: https://crates.io/crates/merkletree looks better actually, try that
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,12 +1,14 @@
-use bitcoin::hashes::Hash as _;
-use bitcoin::{
-    consensus, script::ScriptBuf, secp256k1::Secp256k1, MerkleBlock, PublicKey, Transaction, Txid,
-};
-use ogzkp_core::{
-    recover_pubkey_from_bitcoin_signed_message, start_of_month, verify_header_merkle_proof,
-    OGZKP_MESSAGE_PREFIX,
-};
+use ogzkp_core::verify_header_merkle_proof;
 use risc0_zkvm::guest::env;
+
+use bitcoin::hashes::Hash as _;
+use bitcoin::secp256k1::Secp256k1;
+use bitcoin::sign_message::{signed_msg_hash, MessageSignature, MessageSignatureError};
+use bitcoin::{consensus, script::ScriptBuf, MerkleBlock, PublicKey, Transaction, Txid};
+
+use time::{Date, OffsetDateTime};
+
+pub const OGZKP_MESSAGE_PREFIX: &str = "og-zkp ";
 
 fn pubkey_to_output_script(pubkey: PublicKey, address_type: i32) -> ScriptBuf {
     match address_type {
@@ -32,6 +34,29 @@ fn pubkey_to_output_script(pubkey: PublicKey, address_type: i32) -> ScriptBuf {
         }
         _ => panic!("Invalid address type"),
     }
+}
+
+pub fn recover_pubkey_from_bitcoin_signed_message(
+    signature_bytes: &[u8],
+    message: &str,
+) -> Result<PublicKey, MessageSignatureError> {
+    let signature = MessageSignature::from_slice(signature_bytes)?;
+    let hash = signed_msg_hash(message);
+    let secp = Secp256k1::verification_only();
+    let pubkey = signature.recover_pubkey(&secp, hash)?;
+
+    Ok(pubkey)
+}
+
+pub fn start_of_month(unix_ts: u32) -> u32 {
+    let ts = OffsetDateTime::from_unix_timestamp(unix_ts.into()).unwrap();
+
+    let date = Date::from_calendar_date(ts.year(), ts.month(), 1).unwrap();
+
+    date.with_hms(0, 0, 0)
+        .unwrap()
+        .assume_utc()
+        .unix_timestamp() as u32
 }
 
 fn main() {
