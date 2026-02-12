90313936ac95b869e26c26f71691455113518ea0
Luke Childs <lukechilds123@gmail.com>
2026-02-12 11:58:04 +0700

    Use docker builder

--- methods/build.rs
+++ methods/build.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-    risc0_build::embed_methods();
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let root = std::path::PathBuf::from(manifest_dir).join("..");
+
+    let docker_opts = risc0_build::DockerOptionsBuilder::default()
+        .root_dir(root)
+        .build()
+        .unwrap();
+    let guest_opts = risc0_build::GuestOptionsBuilder::default()
+        .use_docker(docker_opts)
+        .build()
+        .unwrap();
+
+    let mut opts = HashMap::new();
+    opts.insert("ogzkp", guest_opts);
+    risc0_build::embed_methods_with_options(opts);
 }
